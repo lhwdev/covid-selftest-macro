d63bf567996762d9265d5da76845dcd6e800a39c
lhwdev <lhwdev6@outlook.com>
2022-04-17 17:55:52 +0900

    Lifecycle of inactive routes should be <= STARTED

--- app-base/src/main/java/com/lhwdev/selfTestMacro/navigation/composeNavigation.kt
+++ app-base/src/main/java/com/lhwdev/selfTestMacro/navigation/composeNavigation.kt
@@ -5,16 +5,15 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material.SnackbarHost
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.Lifecycle
 import com.lhwdev.selfTestMacro.debug.LocalDebugContext
 import com.lhwdev.selfTestMacro.debug.rememberDebugContext
 import com.lhwdev.selfTestMacro.ui.LocalSnackbarHost
 import com.lhwdev.selfTestMacro.ui.assertConstant
+import com.lhwdev.selfTestMacro.utils.LocalLifecycleState
 
 
 val LocalGlobalNavigator = compositionLocalOf<Navigator> { error("not provided") }
@@ -32,18 +31,32 @@ fun RouteContent(navigator: CurrentNavigator) {
 	val route = navigator.currentRoute
 	
 	Surface(color = if(route[IsOpaque]) Color.Transparent else Color.White) {
-		val snackbarHostState = remember { SnackbarHostState() }
+		val previousLifecycle = LocalLifecycleState.current
+		val lifecycle = remember { // every Route which is inactive should be less than STARTED
+			derivedStateOf {
+				val previous = previousLifecycle.value
+				val isActive = navigator.isActive
+				
+				if(previous == Lifecycle.State.RESUMED) {
+					if(isActive) Lifecycle.State.RESUMED else Lifecycle.State.STARTED
+				} else {
+					previous
+				}
+			}
+		}
 		val debugContext = rememberDebugContext(contextName = route.name ?: "(ComposeRoute)")
 		
 		CompositionLocalProvider(
 			sLocalCurrentNavigator provides navigator,
-			LocalSnackbarHost provides snackbarHostState,
+			LocalLifecycleState provides lifecycle,
+			
+			LocalSnackbarHost provides remember { SnackbarHostState() },
 			LocalDebugContext provides debugContext
 		) {
 			Box {
 				assertConstant(route.content)
 				route.content!!()
-				SnackbarHost(snackbarHostState, modifier = Modifier.safeContentPadding())
+				SnackbarHost(remember { SnackbarHostState() }, modifier = Modifier.safeContentPadding())
 			}
 		}
 	}

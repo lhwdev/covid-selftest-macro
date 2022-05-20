3f740f6a10abb861b8ba3e222a3b8b492389147d
lhwdev <lhwdev6@outlook.com>
2022-05-20 18:11:35 +0900

    🚧 Implement User +alpha (8) - I forgor

--- api/src/main/kotlin/com/lhwdev/selfTestMacro/api/User.kt
+++ api/src/main/kotlin/com/lhwdev/selfTestMacro/api/User.kt
@@ -1,7 +1,7 @@
 package com.lhwdev.selfTestMacro.api
 
 
-public interface User {
+public interface User : UserModel {
 	public val status: ExternalState<UserModel.Status>
 	
 	public suspend fun registerSurvey(answers: AnswersMap): UserModel.Status.SurveyResult
